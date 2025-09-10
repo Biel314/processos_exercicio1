7966851c53c749210bc1549de70dab63277f18b6
Biel314 <gabriel.sordonho@gmail.com>
2025-09-10 14:53:39 -0300

    Finished

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -15,15 +15,16 @@ public class RedesController {
 		return System.getProperty("os.name");
 	}
 	
-
+	//Maquina teste (LINUX MINT)
 	public void ip() {
 		String name = os();
 		String ip, adapt;
+		String[] command = new String[1];
 		
 		if(name.contains("Linux")) {
-			String[] commandLin = {"ifconfig"};
+			command[0] = "ifconfig";
 			try {
-				Process p = Runtime.getRuntime().exec(commandLin); //Interação com o SO e execução de um comando
+				Process p = Runtime.getRuntime().exec(command); //Interação com o SO e execução de um comando
 				InputStream stream = p.getInputStream();
 				InputStreamReader rd =  new InputStreamReader(stream);
 				BufferedReader buffer = new BufferedReader(rd);
@@ -60,9 +61,9 @@ public class RedesController {
 				
 				if(err.contains("2")) {
 					//Mostra todas as linhas de um linux que utiliza ip addr!
-					String[] commandLin2 = {"ip addr"};
+					command[0] = "ip addr";
 					try {
-						Process p = Runtime.getRuntime().exec(commandLin2);
+						Process p = Runtime.getRuntime().exec(command);
 						InputStream stream = p.getInputStream();
 						InputStreamReader rd =  new InputStreamReader(stream);
 						BufferedReader buffer = new BufferedReader(rd);
@@ -86,9 +87,9 @@ public class RedesController {
 			}
 			
 		} else if(name.contains("Windows")) {
-			String[] commandWin = {"ipconfig"};
+			command[0] = "ipconfig";
 			try {
-				Process p = Runtime.getRuntime().exec(commandWin);
+				Process p = Runtime.getRuntime().exec(command);
 				InputStream stream = p.getInputStream();
 				InputStreamReader rd = new InputStreamReader(stream);
 				BufferedReader buffer = new BufferedReader(rd);
@@ -98,6 +99,8 @@ public class RedesController {
 				while(line != null) {
 					if(line.contains("Adaptador")) {
 						adapt = line.trim();
+						
+						System.out.println(adapt);
 					}
 					
 					if(line.contains("IPv4")) {
@@ -120,4 +123,73 @@ public class RedesController {
 			System.out.println("Não identificado");
 	}
 	
+	
+	public void ping() {
+		String name = os();
+		String media = " ";
+		
+		if(name.contains("Windows")) {
+			String[] commandPingWin = {"ping", " ", "-4", " ", "-n", " ", "10", " ", "www.google.com.br"};
+			
+			try {
+				Process p = Runtime.getRuntime().exec(commandPingWin);
+				InputStream stream = p.getInputStream();
+				InputStreamReader rd = new InputStreamReader(stream);
+				BufferedReader buffer = new BufferedReader(rd);
+				
+				String line = buffer.readLine();
+				
+				while(line != null) {
+					if(line.contains(", ")) {
+						media = line.trim().split(", ")[2];
+						media = media.split("= ")[1];
+						System.out.println("Ping no www.google.com.br");
+						System.out.println("A media foi de: " + media);
+					}
+					
+					line = buffer.readLine();
+				}
+				
+				stream.close();
+				rd.close();
+				buffer.close();
+				
+			} catch (IOException err) {
+				System.err.println(err.getMessage());
+			}
+			
+		} else if(name.contains("Linux")) {
+			String[] commandPingLin = {"ping", "-4", "-c", "10", "www.google.com.br"};
+			
+			try {
+				Process p = Runtime.getRuntime().exec(commandPingLin);
+				InputStream stream = p.getInputStream();
+				InputStreamReader rd = new InputStreamReader(stream);
+				BufferedReader buffer = new BufferedReader(rd);
+				
+				String line = buffer.readLine();
+				
+				while(line != null) {
+					if(line.contains("avg")) {
+						media = line.trim().split("= ")[1];
+						media = media.split("/")[1];
+						System.out.println("Ping no www.google.com.br");
+						System.out.println("A media foi de: " + media + " ms");
+					}
+					
+					line = buffer.readLine();
+				}
+				
+				stream.close();
+				rd.close();
+				buffer.close();
+				
+			} catch (IOException err2) {
+				System.err.println(err2.getMessage());
+			}
+			
+		} else {
+			System.out.println("Sistema não identificado!");
+		}
+	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,13 +1,42 @@
 package view;
 
+import java.util.Scanner;
+
 import controller.RedesController;
 
 public class Main {
 
 	public static void main(String[] args) {
 		RedesController redes = new RedesController();
+		Scanner in = new Scanner(System.in);
+		int escolha = 0;
 		
-		redes.ip();
+		System.out.println("Escolha a opção desejada: "
+				+ "\n1- Ver IPv4 e Adaptador"
+				+ "\n2- Ver teste de ping"
+				+ "\n3- Finalizar iteração\n");
+		do {
+		escolha = in.nextInt();
+		
+			switch (escolha) {
+			case 1: 
+				redes.ip();
+				break;
+			case 2: 
+				redes.ping();
+				break;
+			case 3: 
+				System.out.println("Finalizando...");
+				break;
+			default:
+				System.out.println("Escolha invalida!");
+				break;
+			}
+			
+			if(escolha != 3) {
+				System.out.println("\nDeseja ver mais alguma opção? (se nn, digite opção 3)");
+			}
+		} while(escolha != 3);
 	}
 
 }
